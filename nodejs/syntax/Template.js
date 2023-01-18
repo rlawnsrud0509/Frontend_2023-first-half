e596e430d90e4dbe93f20f989d14884f7c04ceed
ddd2341 <rlawnsrud0509@gmail.com>
2023-01-18 23:58:54 +0900

    syntax End..

--- nodejs/syntax/Template.js
+++ nodejs/syntax/Template.js
@@ -4,4 +4,9 @@ const letter =
   name +
   "\n\n definitionem. Ne sea modo reque omnium. Pri simul facilis ad, ut dicta argumentum sit, et minimum delicatissimi sea. Graeci imperdiet nec te, mea ei dicat persecuti. Graece viderer mel eu. Ea timeam legimus nam, nam melius efficiantur delicatissimi an, id volumus iudicabit sit.";
 
+const advLetter = `Doming latine pro ne, no usu tractatos ${name}
+  
+  definitionem. Ne sea modo reque omnium. Pri simul facilis ad, ut dicta argumentum sit, et minimum delicatissimi sea. Graeci imperdiet nec te, mea ei dicat persecuti. Graece viderer mel eu. Ea timeam legimus nam, nam melius efficiantur delicatissimi an, id volumus iudicabit sit.`;
+
 console.log(letter);
+console.log(advLetter);
