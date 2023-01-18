fabbe41645a2a93e86cf42e4155bb53b9f36d93d
ddd2341 <rlawnsrud0509@gmail.com>
2023-01-18 23:54:22 +0900

    syetax

---
+++ nodejs/syntax/Variable2.js
@@ -0,0 +1,7 @@
+const name = "egoing";
+const letter =
+  "Doming latine pro ne, no usu tractatos" +
+  name +
+  "definitionem. Ne sea modo reque omnium. Pri simul facilis ad, ut dicta argumentum sit, et minimum delicatissimi sea. Graeci imperdiet nec te, mea ei dicat persecuti. Graece viderer mel eu. Ea timeam legimus nam, nam melius efficiantur delicatissimi an, id volumus iudicabit sit.";
+
+console.log(letter);
