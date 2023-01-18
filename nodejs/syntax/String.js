52d48fd229f34719064b0a0886bbe8b4bc83349d
ddd2341 <rlawnsrud0509@gmail.com>
2023-01-18 23:43:55 +0900

    syntax

---
+++ nodejs/syntax/String.js
@@ -0,0 +1,7 @@
+console.log(1 + 1);
+console.log("1" + "1");
+console.log("1+1");
+console.log(
+  "Doming latine pro ne, no usu tractatos definitionem. Ne sea modo reque omnium. Pri simul facilis ad, ut dicta argumentum sit, et minimum delicatissimi sea. Graeci imperdiet nec te, mea ei dicat persecuti. Graece viderer mel eu. Ea timeam legimus nam, nam melius efficiantur delicatissimi an, id volumus iudicabit sit."
+    .length
+);
