dffc020a0a67a5d0f01ce07651a9856fb431ef95
ddd2341 <rlawnsrud0509@gmail.com>
2023-01-23 19:54:21 +0900

    first API

--- nodejs/main.js
+++ nodejs/main.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/dog", (req, res) => {
+  res.send("강아지");
+});
+
+app.get("/cat", (req, res) => {
+  res.send("고양이");
+});
+
 app.listen(port, () => {
   console.log(`Express App listening on port ${port}`);
 });
