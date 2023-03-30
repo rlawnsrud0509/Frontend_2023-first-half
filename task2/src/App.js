d9edb4c781dc1cddf2b2a776d4dc5e813f33851c
ddd2341 <rlawnsrud0509@gmail.com>
2023-03-30 15:31:11 +0900

    3/30 React Class

--- task2/src/App.js
+++ task2/src/App.js
@@ -2,24 +2,28 @@ import NavBar from "./Components/navBar";
 import Hero from "./Components/Hero";
 import Card from "./Components/Card";
 import "./App.css";
+import data from "./Components/data";
 
 function App() {
+  const cardArray = data.map((data) => {
+    return (
+      <Card
+        img={`image${data.id}`}
+        rating={data.stats.rating}
+        reviewCount={data.stats.reviewCount}
+        country={data.location}
+        title={data.title}
+        price={data.price}
+      ></Card>
+    );
+  });
+
   return (
     <div className="container">
       <NavBar></NavBar>
+
       <Hero></Hero>
-      <div className="Card">
-        <Card
-          img={"image1"}
-          rating={5.0}
-          reviewCount={6}
-          country={"USA"}
-          title={"Life Lessons with Katie Zaferes"}
-          price={136}
-        ></Card>
-        <Card></Card>
-        <Card></Card>
-      </div>
+      <div className="Card">{cardArray}</div>
     </div>
   );
 }
