720e274d67c02478485fa88faca4f43594d77598
ddd2341 <rlawnsrud0509@gmail.com>
2023-04-13 16:03:32 +0900

    23/04/13 react class

--- task10/src/Form.js
+++ task10/src/Form.js
@@ -1,18 +1,50 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [text, setText] = useState();
+  const [text, setText] = useState({
+    first: "",
+    last: "",
+    email: "",
+  });
+
+  function handleChange(event) {
+    setText((prev) => {
+      return {
+        ...prev,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+  //   e.target.name === "first"
+  //   ? (text.firstName = e.target.value)
+  //   : e.target.name === "last"
+  //   ? (text.lastName = e.target.value)
+  //   : (text.email = e.target.value)
 
   return (
     <form>
       <input
+        name="first"
         type="text"
         placeholder="First Name"
-        onChange={(e) => {
-          setText(e.target.value);
-        }}
+        onChange={handleChange}
       />
-      <h1>{text}</h1>
+      <input
+        name="last"
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+      />
+      <input
+        name="email"
+        type="text"
+        placeholder="Email"
+        onChange={handleChange}
+      />
+      <h1>
+        Name : {text.first} {text.last}
+      </h1>
+      <h1>Email : {text.email}</h1>
     </form>
   );
 }
