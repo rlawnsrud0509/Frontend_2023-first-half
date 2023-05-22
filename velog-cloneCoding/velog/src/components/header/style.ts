c54905166b70dbfea72ebd4417ea408d9b16995e
ddd2341 <rlawnsrud0509@gmail.com>
2023-05-22 20:28:23 +0900

    2023/05/22 React task

--- velog-cloneCoding/velog/src/components/header/style.ts
+++ velog-cloneCoding/velog/src/components/header/style.ts
@@ -41,7 +41,7 @@ export const HeaderContainerOptions = styled.div`
   height: min-content;
 
   display: flex;
-  gap: 10px;
+  gap: 5px;
   align-items: center;
   font-size: 1rem;
 `;
@@ -59,15 +59,16 @@ export const ChangeTheme = keyframes`
 `;
 
 export const ThemeBtn = styled.button<{ darkTheme: boolean }>`
-  width: 30px;
-  height: 30px;
+  width: 40px;
+  height: 40px;
 
   border: none;
   border-radius: 20px;
 
   background: url(${(props) => props.darkTheme ? C.DarkTheme.ChangeBtnUrl : C.LightTheme.ChangeBtnUrl});
   background-position: center;
-  background-size: cover;
+  background-size: 25px;
+  background-repeat: no-repeat;
 
   transition-timing-function: ease-out;
   transition-duration: 0.1s;
@@ -80,13 +81,16 @@ export const ThemeBtn = styled.button<{ darkTheme: boolean }>`
 `;
 
 export const SearchBtn = styled.div<{ darkTheme: boolean }>`
-  width: 30px;
-  height: 30px;
+  width: 40px;
+  height: 40px;
 
   border: none;
   border-radius: 20px;
 
   background: url(${(props) => props.darkTheme ? C.DarkTheme.SearchBtnUrl : C.LightTheme.SearchBtnUrl});
+  background-position: center;
+  background-size: 20px;
+  background-repeat: no-repeat;
 
   &:hover {
     cursor: pointer;
