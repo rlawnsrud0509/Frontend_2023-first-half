8584309f2116309223fb7769388b7d2a20aedf4b
ddd2341 <rlawnsrud0509@gmail.com>
2023-06-03 22:16:43 +0900

    오어스마스터

--- googlelogin/src/components/login.tsx
+++ googlelogin/src/components/login.tsx
@@ -1,8 +1,13 @@
 import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 const Login = () => {
-  const onSuccess = (res: object) => {
-    console.log("로그인 성공~~!", res);
+  const onSuccess = (res: any) => {
+    console.log("로그인 성공~~!", res.credential);
+
+    axios.post("ocalhost:8080/api/login/google", {
+      token: res.credential,
+    });
   };
 
   console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
